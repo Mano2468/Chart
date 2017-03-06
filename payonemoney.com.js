326b7a0bc874e9ab033df05713d3eec12e84b70a
Manoranjan Nayak <Manoranjan Nayak>
2017-03-06 18:44:46 +0530

    payonemoneychanges

--- payonemoney.com.js
+++ payonemoney.com.js
@@ -1,9 +1,8 @@
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/56cea6f7850752be2097f7b8/default';
+s1.src='https://embed.tawk.to/58bd4ba42dfdd91cf6ec9628/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);

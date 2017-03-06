2d00a4640255e228ca1bf483bdb97e29248b3211
Manoranjan Nayak <Manoranjan Nayak>
2017-03-06 18:31:47 +0530

    test

--- mrmoneytransfer.com.js
+++ mrmoneytransfer.com.js
@@ -1,10 +1,8 @@
-
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/58bd4ba42dfdd91cf6ec9628/default';
+s1.src='https://embed.tawk.to/58bd3e8a5b89e2149e11cb9b/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);

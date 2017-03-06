a050e4caf8fc0d610e3e45bf4d1e0a078e0ce252
Manoranjan Nayak <Manoranjan Nayak>
2017-03-06 19:27:03 +0530

    mrmoneytransfer chart changes

--- mrmoneytransfer.com.js
+++ mrmoneytransfer.com.js
@@ -2,7 +2,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/58bd3e8a5b89e2149e11cb9b/default';
+s1.src='https://embed.tawk.to/56cea6f7850752be2097f7b8/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
